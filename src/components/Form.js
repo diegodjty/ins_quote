708fc6ce427fba5b871e7fb02a3e04616e51f5b6
Diego Taveras <diegotaveras2@outlook.com>
2020-03-23 12:19:24 -0400

    Add style components to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,62 @@
 import React from 'react'
 
+import styled from '@emotion/styled';
+
+const Field = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center; 
+`;
+
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`;
+
+const Button = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover{
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
+
 const Form = () => {
     return (
         <form>
-            <div>
-                <label htmlFor=""> Make </label>
-                <select name="" id="">
+            <Field>
+                <Label htmlFor=""> Make </Label>
+                <Select name="" id="">
                     <option value="">--- Select ----</option>
                     <option value="American">American</option>
                     <option value="European">European</option>
                     <option value="Asian">Asian</option>
-                </select>
-            </div>
-            <div>
-                <label htmlFor=""> Year </label>
-                <select name="" id="">
+                </Select>
+            </Field>
+            <Field>
+                <Label htmlFor=""> Year </Label>
+                <Select name="" id="">
                     <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -26,25 +68,25 @@ const Form = () => {
                     <option value="2014">2014</option>
                     <option value="2013">2013</option>
                     <option value="2012">2012</option>
-                </select>
-            </div>
+                </Select>
+            </Field>
 
-            <div>
-                <label>Plan</label>
-                <input 
+            <Field>
+                <Label>Plan</Label>
+                <InputRadio 
                     type="radio"
                     name="plan"
                     value="basic"
                 /> Basic
-                <input 
+                <InputRadio 
                     type="radio"
                     name="plan"
                     value="full"
                 /> Full
                 
-            </div>
+            </Field>
 
-            <button type="button">Quoute</button>
+            <Button type="button">Quoute</Button>
             
         </form>
     )
